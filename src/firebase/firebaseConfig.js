c4c93091adf18ed78b71fee8bdc747434bfdd9eb
Camila-TS <camilathompson@gmail.com>
2022-10-14 18:18:05 -0300

    agregando config de firebase, sin inicializarla

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCvZDLjhVmewF3q-WJkLQ3c7atMDRdfWOE",
+    authDomain: "anotalapp.firebaseapp.com",
+    projectId: "anotalapp",
+    storageBucket: "anotalapp.appspot.com",
+    messagingSenderId: "450772797409",
+    appId: "1:450772797409:web:4ff3c94f6d229eb6c0f4a0"
+  };
+
+  export default firebaseConfig;
